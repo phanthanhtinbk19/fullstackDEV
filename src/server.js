a173aa5608357eea334ebb4da8e71ec65f0bdc34
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2022-02-13 09:24:57 +0700

    FS

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import iniWebRoutes from "./route/web";
+require("dotenv").config();
+let app = express();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+viewEngine(app);
+iniWebRoutes(app);
+
+let port = process.env.PORT || 6969;
+app.listen(port, () => {
+	console.log("backend modejs is running on port " + port);
+});
